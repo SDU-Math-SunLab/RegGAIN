6a105f3d45c8e6c40a72ccdb45704b81b7cb4993
SDU-Math-SunLab <dcsun@sdu.edu.cn>
2025-09-15 08:48:16 +0800

    Update __init__.py

--- RegGAIN_script/__init__.py
+++ RegGAIN_script/__init__.py
@@ -2,4 +2,3 @@ from .run_RegGAIN import run_reggain
 from .utils import data_preparation, get_PYG_data
 from .evaluation_utils import calculate_epr_aupr
 from .Model import Model, ConvModel
-from .result_analysis import plot_gene_embedding_clusters
